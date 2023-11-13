99339537727f6e28f63112c6c01a6ca8d0c3756a
Sirgeeyou <bojorsergiu@gmail.com>
2023-11-13 17:15:48 +0200

    feef

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -8,7 +8,11 @@ export const HeroSection = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <img src={heropicture} className="max-w-sm rounded-lg shadow-2xl" />
+        <img
+          src={heropicture}
+          className="max-w-sm rounded-lg shadow-2xl"
+          alt="text"
+        />
         <div>
           <h1 className="text-5xl font-bold">Hello there!</h1>
           <p className="py-6">
