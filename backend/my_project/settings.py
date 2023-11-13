2221a6a4e41a1e10ca20d5e63f73fce56af6140f
Sirgeeyou <bojorsergiu@gmail.com>
2023-11-13 16:17:13 +0200

    hehehe

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c))rn^3)f(njo5^p%e6*9wsw38v52l^f(-1c6p7ar$f$!rll-k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel-app', '.now.sh']
 
 
 
@@ -149,9 +149,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
